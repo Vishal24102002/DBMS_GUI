447466c59d01bf6d640111db4c67a38b788b2991
Vishal sharma <103731436+Vishal24102002@users.noreply.github.com>
2023-03-05 09:26:46 +0530

    Update Maincode.py

--- Maincode.py
+++ Maincode.py
@@ -1,10 +1,14 @@
 try:
-  Import mysql-connector
-  Import tkinter as tk
+  import mysql-connector
+  import tkinter as tk
+  from tkinter import messagebox
 except:
-  Import pip
+  import pip
   pip.main(['install','tk'])
   pip.main(['install','mysql-connector'])
+  import mysql-connector
+  import tkinter as tk
+  from tkinter import messagebox
 
 def connect():
   mydb=mysql.connector.connect(host="localhost",user="root",password="vishal")
@@ -15,7 +19,26 @@ def connect():
     mycursor.execute("create database aravalian")
     mycursor.execute("use aravalian")
     print("connected successfully")
+  Start_label.destroy()
   connection_button.destroy()
+
+def main():
+  name_label=label(pradeep,text="teacher-name",fg="white",font=('calibre',10,'normal'))
+  name_entry=Entry(pradeep,textvariable=user,font=('calibre',10,'normal'))
+  id_label=label(pradeep,text="Employee-ID",fg="white",font=('calibre',10,'normal'))
+  id_entry=Entry(pradeep,textvariable=ID,font=('calibre',10,'normal'))
+  name_label.grid()
+  name_entry.grid()
+  id_label.grid()
+  id_entry.grid()
+  search_button=Button(pradeep,text="Search",cursor="hand2")
+  search_button.grid()
+
+def alertbox():
+  messagebox.askyesno("confirmation","Do you want to save the data")
+  mycursor.commit()
+
+global vishal,pradeep,mycursor,mydb
 
 vishal=Tk()
 vishal.title("ARAVALIANS")
