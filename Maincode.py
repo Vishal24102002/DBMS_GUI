7b9268a50899190f50f1270a02389f88d82e52cf
Vishal sharma <103731436+Vishal24102002@users.noreply.github.com>
2023-03-05 23:53:12 +0530

    Update Maincode.py

--- Maincode.py
+++ Maincode.py
@@ -10,6 +10,14 @@ except:
   import tkinter as tk
   from tkinter import messagebox
 
+def speak(text):
+    engine=pt.init()
+    engine.say(text)
+    Voices=engine.getProperty('voices') 
+    print(Voices) 
+    engine.setProperty('voice',Voices[0].id) 
+    engine.runAndWait()
+
 def connect():
   mydb=mysql.connector.connect(host="localhost",user="root",password="vishal")
   mycursor=mydb.cursor()
@@ -19,6 +27,7 @@ def connect():
     mycursor.execute("create database aravalian")
     mycursor.execute("use aravalian")
     print("connected successfully")
+    speak("connected successfully")
   Start_label.destroy()
   connection_button.destroy()
 
@@ -47,8 +56,8 @@ vishal.minsize(450,400)
 vishal.configure(bg="#1b03a3")
 pradeep=Frame(vishal,bg="black",height=380,width=430))
 pradeep.pack()
-Start_label=label(pradeep,text="welcome to Aravality",fg="white")
+global Start_label=label(pradeep,text="welcome to Aravality",fg="white")
 Start_label.place(relx=0.5,rely=1,anchor='center')
-connection_button=Button(pradeep,text='connect',activebackground="blue",activeforground="red",command=connect)
+global connection_button=Button(pradeep,text='connect',activebackground="blue",activeforground="red",command=connect)
 connection_button.place(relx=0.5,rely=1,anchor='center')
 vishal.mainloop()
