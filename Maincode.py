fee16143ed9fbcfa3872cb0e6d7f73e3a5cd59a6
Vishal sharma <103731436+Vishal24102002@users.noreply.github.com>
2023-03-06 15:09:19 +0530

    Update Maincode.py

--- Maincode.py
+++ Maincode.py
@@ -1,14 +1,14 @@
 try:
-  import mysql-connector
-  import tkinter as tk
-  from tkinter import messagebox
+    import mysql-connector
+    import tkinter as tk
+    from tkinter import messagebox
 except:
-  import pip
-  pip.main(['install','tk'])
-  pip.main(['install','mysql-connector'])
-  import mysql-connector
-  import tkinter as tk
-  from tkinter import messagebox
+    import pip
+    pip.main(['install','tk'])
+    pip.main(['install','mysql-connector'])
+    import mysql-connector
+    import tkinter as tk
+    from tkinter import messagebox
 
 def speak(text):
     engine=pt.init()
@@ -19,33 +19,33 @@ def speak(text):
     engine.runAndWait()
 
 def connect():
-  mydb=mysql.connector.connect(host="localhost",user="root",password="vishal")
-  mycursor=mydb.cursor()
-  try:
-    mycursor.execute("use aravalian")
-  except:
-    mycursor.execute("create database aravalian")
-    mycursor.execute("use aravalian")
-    print("connected successfully")
-    speak("connected successfully")
-  Start_label.destroy()
-  connection_button.destroy()
+    mydb=mysql.connector.connect(host="localhost",user="root",password="vishal")
+    mycursor=mydb.cursor()
+    try:
+      mycursor.execute("use aravalian")
+    except:
+      mycursor.execute("create database aravalian")
+      mycursor.execute("use aravalian")
+      print("connected successfully")
+      speak("connected successfully")
+    Start_label.destroy()
+    connection_button.destroy()
 
 def main():
-  name_label=label(pradeep,text="teacher-name",fg="white",font=('calibre',10,'normal'))
-  name_entry=Entry(pradeep,textvariable=user,font=('calibre',10,'normal'))
-  id_label=label(pradeep,text="Employee-ID",fg="white",font=('calibre',10,'normal'))
-  id_entry=Entry(pradeep,textvariable=ID,font=('calibre',10,'normal'))
-  name_label.grid(row=0,column=0)
-  name_entry.grid(row=0,column=1)
-  id_label.grid(row=1,column=0)
-  id_entry.grid(row=1,column=1)
-  search_button=Button(pradeep,text="Search",cursor="hand2",command=detail)
-  search_button.grid(row=2,column=0,columnspan=2)
+    name_label=label(pradeep,text="teacher-name",fg="white",font=('calibre',10,'normal'))
+    name_entry=Entry(pradeep,textvariable=user,font=('calibre',10,'normal'))
+    id_label=label(pradeep,text="Employee-ID",fg="white",font=('calibre',10,'normal'))
+    id_entry=Entry(pradeep,textvariable=ID,font=('calibre',10,'normal'))
+    name_label.grid(row=0,column=0)
+    name_entry.grid(row=0,column=1)
+    id_label.grid(row=1,column=0)
+    id_entry.grid(row=1,column=1)
+    search_button=Button(pradeep,text="Search",cursor="hand2",command=detail)
+    search_button.grid(row=2,column=0,columnspan=2)
 
 def alertbox():
-  messagebox.askyesno("confirmation","Do you want to save the data")
-  mycursor.commit()
+    messagebox.askyesno("confirmation","Do you want to save the data")
+    mycursor.commit()
 
 global vishal,pradeep,mycursor,mydb
 
