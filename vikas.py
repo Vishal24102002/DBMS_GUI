81479f5425d5d418fc7fcbbe8d540a4023be2825
Vishal sharma <103731436+Vishal24102002@users.noreply.github.com>
2023-03-13 18:29:59 +0530

    Update vikas.py

--- vikas.py
+++ vikas.py
@@ -15,9 +15,9 @@ Vikas.minsize(300,250)
 Vikas.title("Speach converter")
 Vikas.configure(bg="black")
 
-def speak(text):
+def speak():
    Engine=pt.init()
-   Engine.say(text)
+   Engine.say(Entry.get())
    Voices=engine.getProperty('voices') 
    print(Voices) 
    engine.setProperty('voice',Voices[1].id) 
@@ -31,11 +31,12 @@ entry_label.grid(row=0,column=0)
 entry1_label(Vikas,text=" ")
 entry1_label.grid(row=0,column=4)
 
+Entry=StringVar()
 
 Text=Entry(Vikas,textvariable=Entry,font=('calibre',12,'normal'))
 Text.grid(row=4,column=3,columnspan=4)
 
-Speak=Button(vikas,text="convert",activebackground="blue",command=lambda:speak(Entry.get()))
+Speak=Button(vikas,text="convert",activebackground="blue",command=speak)
 Speak.grid(row=5,column=3,columnspan=4)
 
 Vikas.mainloop()
